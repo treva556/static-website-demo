6b2d3c09b813eefdd84136de1b4b8122f7a21dfc
treva556 <marktreva92@gmail.com>
2023-04-30 00:28:30 +0300

    side bar removed

--- src/components/Card2.js
+++ src/components/Card2.js
@@ -18,10 +18,10 @@ export default function Card2() {
         </a>
         <div class="p-5">
             <a href="#">
-                <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white text-center">  Football</h5>
+                <h5 class="mb-2 text-2xl font-bold tracking-tight text-[#FEA4B9] dark:text-white text-center">  Football</h5>
             </a>
-            <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">  football, also called association football or soccer, game in which two teams of 11 players, using any part of their bodies except their hands and arms, try to maneuver the ball into the opposing team’s goal. Only the goalkeeper is permitted to handle the ball and may do so only within the penalty area surrounding the goal. The team that scores more goals wins. </p>
-            <a href="#" class="inline-flex items-center px-5 py-3 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 items-center">
+            <p class="mb-3 font-normal text-[#FEA4B9] dark:text-gray-400">  football, also called association football or soccer, game in which two teams of 11 players, using any part of their bodies except their hands and arms, try to maneuver the ball into the opposing team’s goal. Only the goalkeeper is permitted to handle the ball and may do so only within the penalty area surrounding the goal. The team that scores more goals wins. </p>
+            <a href="#" class="inline-flex items-center px-5 py-3 text-sm font-medium text-center text-white bg-[#FEA4B9] rounded-lg hover:bg-[#FEA4B9] focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-[#FEA4B9] dark:hover:bg-[#FEA4B9] dark:focus:ring-[#FEA4B9] items-center">
                 Read more
                 <svg aria-hidden="true" class="w-4 h-4 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
             </a>
--- src/components/Header.js
+++ src/components/Header.js
@@ -12,7 +12,7 @@ function Header(){
       </a>
     </div>
     <div class="flex lg:hidden">
-      <button type="button" class="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700">
+      <button type="button" class="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-[#FEA4B9]">
         <span class="sr-only">Open main menu</span>
         <svg class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
           <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
@@ -32,13 +32,13 @@ function Header(){
       <a href="#" class="text-sm font-semibold leading-6 text-gray-900">Company</a>
     </div> */}
     <div class="hidden lg:flex lg:flex-1 lg:justify-end">
-      <a href="#" class="text-sm font-semibold leading-6 text-gray-900">Log in <span aria-hidden="true">&rarr;</span></a>
-      <a href="#" class="text-sm font-semibold leading-6 text-gray-900">Dashboard<span aria-hidden="true">&rarr;</span></a>
-      <a href="#" class="text-sm font-semibold leading-6 text-gray-900">about us<span aria-hidden="true">&rarr;</span></a>
+      <a href="#" class="text-sm font-semibold leading-6 text-[#FEA4B9]">Log in <span aria-hidden="true">&rarr;</span></a>
+      <a href="#" class="text-sm font-semibold leading-6 text-[#FEA4B9]">Dashboard<span aria-hidden="true">&rarr;</span></a>
+      <a href="#" class="text-sm font-semibold leading-6 text-[#FEA4B9]">about us<span aria-hidden="true">&rarr;</span></a>
 
     </div>
   </nav>
-  <div class="lg:hidden" role="dialog" aria-modal="true">
+    {/* <div class="lg:hidden" role="dialog" aria-modal="true">
     <div class="fixed inset-0 z-10"></div>
     <div class="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
       <div class="flex items-center justify-between">
@@ -51,9 +51,9 @@ function Header(){
           <svg class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
             <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
           </svg>
-        </button>
-      </div>
-      <div class="mt-6 flow-root">
+        </button> */}
+      {/* </div>
+      {/* <div class="mt-6 flow-root">
         <div class="-my-6 divide-y divide-gray-500/10">
           <div class="space-y-2 py-6">
             <div class="-mx-3">
@@ -71,10 +71,10 @@ function Header(){
           <div class="py-6">
             <a href="#" class="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">Log out</a>
           </div>
-        </div>
-      </div>
-    </div>
-  </div>
+        </div>*/}
+       {/* </div> 
+    </div> 
+  </div> */}
 </header>
         </>
     )
